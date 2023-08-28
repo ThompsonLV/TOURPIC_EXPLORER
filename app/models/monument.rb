0bc28f0326caf960253bff8569c1facffde63a8f
Thomas Le Veo <thomasleveo@gmail.com>
2023-08-28 18:00:17 +0200

    bolong et has_many fait

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -1,2 +1,4 @@
 class Monument < ApplicationRecord
+  has_many :questions
+  has_many :user_monuments
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :monument
+  has_many :answers
 end
