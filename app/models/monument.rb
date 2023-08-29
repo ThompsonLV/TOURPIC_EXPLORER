8caf388cc8bd6a194459dcc0029c98e8a4020c59
Charles de Montfalcon <charlesdemont@hotmail.fr>
2023-08-29 14:14:51 +0200

    modif

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -1,4 +1,9 @@
 class Monument < ApplicationRecord
   has_many :questions
   has_many :user_monuments
+  validates :photos, presence: true
+  validates :title, presence: true
+  validates :address, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
