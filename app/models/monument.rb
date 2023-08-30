f8a57babf9372d2195681d401f503222ba2e6ade
Thomas Le Veo <thomasleveo@gmail.com>
2023-08-30 22:34:01 +0200

    Seed avec AI en cours pb d'url

--- app/models/monument.rb
+++ app/models/monument.rb
@@ -3,7 +3,7 @@ class Monument < ApplicationRecord
   has_many :user_monuments, dependent: :destroy
   # validates :photos, presence: true
   validates :title, presence: true
-  validates :address, presence: true
+  # validates :address, presence: true
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
