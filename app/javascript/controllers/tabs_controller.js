5f7aa7798f249536da5cf947fbb27019894eff5f
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-04 22:39:49 +0200

    Tabs pseudo class active

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
-  static targets = ["unlockedMonuments", "myMonuments", "classement"]
+  static targets = ["unlockedMonuments", "myMonuments", "classement", "title1", "title2", "title3"]
   connect() {
 
   }
@@ -13,6 +13,9 @@ export default class extends Controller {
     this.unlockedMonumentsTarget.classList.remove("d-none");
     this.myMonumentsTarget.classList.add("d-none");
     this.classementTarget.classList.add("d-none")
+    this.title1Target.classList.add("underline");
+    this.title2Target.classList.remove("underline");
+    this.title3Target.classList.remove("underline");
   }
 
   toggle2(event){
@@ -21,6 +24,9 @@ export default class extends Controller {
     this.myMonumentsTarget.classList.remove("d-none");
     this.unlockedMonumentsTarget.classList.add("d-none");
     this.classementTarget.classList.add("d-none")
+    this.title1Target.classList.remove("underline");
+    this.title2Target.classList.add("underline");
+    this.title3Target.classList.remove("underline");
   }
 
   toggle3(event){
@@ -29,5 +35,8 @@ export default class extends Controller {
     this.classementTarget.classList.remove("d-none");
     this.unlockedMonumentsTarget.classList.add("d-none");
     this.myMonumentsTarget.classList.add("d-none")
+    this.title1Target.classList.remove("underline");
+    this.title2Target.classList.remove("underline");
+    this.title3Target.classList.add("underline");
   }
 }
