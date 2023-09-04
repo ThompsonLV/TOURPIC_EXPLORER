728a1a969ec8dfe391cd72c794a1ef8eff073b9a
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-04 22:21:20 +0200

    Tabs test pseudo class active

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,7 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
+  static targets = ["unlockedMonuments", "myMonuments", "classement"]
   connect() {
-    console.log("hello from tabs");
+
+  }
+
+  toggle1(event){
+    event.preventDefault()
+    console.log("element1");
+    this.unlockedMonumentsTarget.classList.remove("d-none");
+    this.myMonumentsTarget.classList.add("d-none");
+    this.classementTarget.classList.add("d-none")
+  }
+
+  toggle2(event){
+    event.preventDefault()
+    console.log("element2");
+    this.myMonumentsTarget.classList.remove("d-none");
+    this.unlockedMonumentsTarget.classList.add("d-none");
+    this.classementTarget.classList.add("d-none")
+  }
+
+  toggle3(event){
+    event.preventDefault()
+    console.log("element3");
+    this.classementTarget.classList.remove("d-none");
+    this.unlockedMonumentsTarget.classList.add("d-none");
+    this.myMonumentsTarget.classList.add("d-none")
   }
 }
