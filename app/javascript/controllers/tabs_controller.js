5a5e07d2ee3bf72b76664aaa05cc607e7ab752f9
Charles de Montfalcon <charlesdemont@hotmail.fr>
2023-09-05 15:52:52 +0200

    modif

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tabs"
 export default class extends Controller {
-  static targets = ["unlockedMonuments", "myMonuments", "classement", "title1", "title2", "title3"]
+  static targets = ["unlockedMonuments", "myMonuments", "classement", "puzzle", "title1", "title2", "title3", "title4"]
   connect() {
 
   }
@@ -12,19 +12,24 @@ export default class extends Controller {
     this.unlockedMonumentsTarget.classList.remove("d-none");
     this.myMonumentsTarget.classList.add("d-none");
     this.classementTarget.classList.add("d-none")
+    this.puzzleTarget.classList.add("d-none")
     this.title1Target.classList.add("underline");
     this.title2Target.classList.remove("underline");
     this.title3Target.classList.remove("underline");
+    this.title4Target.classList.remove("underline");
   }
+
 
   toggle2(event){
     event.preventDefault()
     this.myMonumentsTarget.classList.remove("d-none");
     this.unlockedMonumentsTarget.classList.add("d-none");
     this.classementTarget.classList.add("d-none")
+    this.puzzleTarget.classList.add("d-none")
     this.title1Target.classList.remove("underline");
     this.title2Target.classList.add("underline");
     this.title3Target.classList.remove("underline");
+    this.title4Target.classList.remove("underline");
   }
 
   toggle3(event){
@@ -32,8 +37,21 @@ export default class extends Controller {
     this.classementTarget.classList.remove("d-none");
     this.unlockedMonumentsTarget.classList.add("d-none");
     this.myMonumentsTarget.classList.add("d-none")
+    this.puzzleTarget.classList.add("d-none")
     this.title1Target.classList.remove("underline");
     this.title2Target.classList.remove("underline");
     this.title3Target.classList.add("underline");
+    this.title4Target.classList.remove("underline");
+  }
+  toggle4(event){
+    event.preventDefault()
+    this.classementTarget.classList.add("d-none");
+    this.unlockedMonumentsTarget.classList.add("d-none");
+    this.myMonumentsTarget.classList.add("d-none")
+    this.puzzleTarget.classList.remove("d-none")
+    this.title1Target.classList.remove("underline");
+    this.title2Target.classList.remove("underline");
+    this.title3Target.classList.remove("underline");
+    this.title4Target.classList.add("underline");
   }
 }
