cb47569e36fad80efeb05c6e587e1e8e1e7b0732
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-06 11:32:53 +0200

    Seed terminée

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :answers, through: :user_answers
   has_many :user_monuments, dependent: :destroy
   has_many :monuments, through: :user_monuments
+  has_one_attached :photo
 end
