2fdf1388ce1409cd4ca887d280138574afa50e7e
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-06 15:57:59 +0200

    navbar modifié

--- app/javascript/controllers/darken_link_controller.js
+++ app/javascript/controllers/darken_link_controller.js
@@ -5,8 +5,24 @@ export default class extends Controller {
   static targets = ["link1", "link2", "link3"]
 
   connect() {
-    setTimeout(function(){
-  }, 2000);
-    console.log("URL de la page : ", window.location.pathname);
+    console.log(window.location.pathname);
+    const path = window.location.pathname;
+    switch (path){
+      case "/":
+        this.link1Target.classList.add("dark-blue")
+        this.link2Target.classList.remove("dark-blue")
+        this.link3Target.classList.remove("dark-blue")
+        break;
+      case "/my_profil":
+        this.link1Target.classList.remove("dark-blue")
+        this.link2Target.classList.add("dark-blue")
+        this.link3Target.classList.remove("dark-blue")
+        break;
+      case "/parameters":
+        this.link1Target.classList.remove("dark-blue")
+        this.link2Target.classList.remove("dark-blue")
+        this.link3Target.classList.add("dark-blue")
+        break;
+    }
   }
 }
