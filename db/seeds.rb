24180874e139a7aac77f3ed7086da178ca0b0724
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-07 11:29:23 +0200

    Seed faite avec les bons avatars

--- db/seeds.rb
+++ db/seeds.rb
@@ -90,27 +90,27 @@ Monument.create!(
 p "Création des comptes"
 p '------------------------'
 
-avatar = Cloudinary::Api.resources(type: 'upload', prefix: "avatars/")
+avatar = Cloudinary::Api.resources(type: 'upload', prefix: "teams_avatar")
 
 @thomas = User.new(email: "thomas@gmail.com", first_name: "Thomas", last_name: "Le Véo", password: 'azerty', password_confirmation: 'azerty')
-random_avatar = avatar["resources"].sample
-random_avatar_url = Cloudinary::Utils.cloudinary_url(random_avatar['public_id'], width: 300, height: 300, crop: 'fill')
-image_data = URI.open(random_avatar_url).read
-@thomas.photo.attach(io: StringIO.new(image_data), filename: "theatre.jpeg", content_type: "image/jpeg")
+thomas_avatar = avatar["resources"].find { |resource| resource["public_id"] == "teams_avatar/thomas" }
+thomas_avatar_url = Cloudinary::Utils.cloudinary_url(thomas_avatar['public_id'], width: 300, height: 300, crop: 'fill')
+image_data = URI.open(thomas_avatar_url).read
+@thomas.photo.attach(io: StringIO.new(image_data), filename: "thomas.jpeg", content_type: "image/jpeg")
 @thomas.save
 
 @charles = User.new(email: "charles@gmail.com", first_name: "Charles", last_name: "DeMont", password: 'azerty', password_confirmation: 'azerty')
-random_avatar = avatar["resources"].sample
-random_avatar_url = Cloudinary::Utils.cloudinary_url(random_avatar['public_id'], width: 300, height: 300, crop: 'fill')
-image_data = URI.open(random_avatar_url).read
-@charles.photo.attach(io: StringIO.new(image_data), filename: "theatre.jpeg", content_type: "image/jpeg")
+charles_avatar = avatar["resources"].find { |resource| resource["public_id"] == "teams_avatar/charles" }
+charles_avatar_url = Cloudinary::Utils.cloudinary_url(charles_avatar['public_id'], width: 300, height: 300, crop: 'fill')
+image_data = URI.open(charles_avatar_url).read
+@charles.photo.attach(io: StringIO.new(image_data), filename: "charles.jpeg", content_type: "image/jpeg")
 @charles.save
 
 @lazari = User.new(email: "lazari@gmail.com", first_name: "Lazari", last_name: "Kacimi", password: 'azerty', password_confirmation: 'azerty')
-random_avatar = avatar["resources"].sample
-random_avatar_url = Cloudinary::Utils.cloudinary_url(random_avatar['public_id'], width: 300, height: 300, crop: 'fill')
-image_data = URI.open(random_avatar_url).read
-@lazari.photo.attach(io: StringIO.new(image_data), filename: "theatre.jpeg", content_type: "image/jpeg")
+lazari_avatar = avatar["resources"].find { |resource| resource["public_id"] == "teams_avatar/lazari" }
+lazari_avatar_url = Cloudinary::Utils.cloudinary_url(lazari_avatar['public_id'], width: 300, height: 300, crop: 'fill')
+image_data = URI.open(lazari_avatar_url).read
+@lazari.photo.attach(io: StringIO.new(image_data), filename: "lazari.jpeg", content_type: "image/jpeg")
 @lazari.save
 
 5.times do
@@ -139,7 +139,7 @@ a = 1
 users.each do |user|
   p "User #{a}"
   (1..10).to_a.sample.times do
-
+    
     random_monument = monuments.sample
     new_user_monument = UserMonument.create!(user: user, monument: random_monument, favoris: false)
 
@@ -150,9 +150,9 @@ users.each do |user|
     new_user_monument.photos.attach(io: StringIO.new(image_data), filename: "theatre.jpeg", content_type: "image/jpeg")
     new_user_monument.save
   end
-  user.score = (UserMonument.where(user_id: user.id).count) * 500
+  user.score = UserMonument.where(user_id: user.id).count * 500
+  user.save
   a += 1
-
 end
 
 p '------------------------'
