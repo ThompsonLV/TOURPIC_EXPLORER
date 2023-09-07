f17120aac4f88bd7615dc01174eee8e3bce08bdf
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-07 12:37:53 +0200

    modification my_profil z-index linear background

--- app/javascript/controllers/profil_controller.js
+++ app/javascript/controllers/profil_controller.js
@@ -5,14 +5,8 @@ export default class extends Controller {
   static targets = ["avatar"];
 
   connect() {
-    console.log(this.avatarTarget);
-
-    const originalWidth  = this.avatarTarget.style.width;
-    const originalHeight = this.avatarTarget.style.height;
-
     document.addEventListener('scroll', (e) => {
       const scrollY = window.scrollY;
-      console.log(scrollY);
       if (scrollY > 200) {
         // this.avatarTarget.style.width = '60px';
         // this.avatarTarget.style.height = '60px';
