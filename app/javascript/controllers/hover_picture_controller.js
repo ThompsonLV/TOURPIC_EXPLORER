fa54566a7eab3350ebf6593cf9b7c258810f68ab
Thomas Le Veo <thomasleveo@gmail.com>
2023-09-07 18:54:09 +0200

    hover picture avec timer

--- app/javascript/controllers/hover_picture_controller.js
+++ app/javascript/controllers/hover_picture_controller.js
@@ -7,21 +7,47 @@ export default class extends Controller {
   connect() {
     console.log("hover picture");
     console.log(this.photoTarget.src);
+
+    // this.photoTarget.addEventListener("touchstart", (event) => {
+    //   clearTimeout(timer)
+    //   const url_image = event.currentTarget.src;
+    //   timer = setTimeout((event) => {
+    //       this.bluredTarget.classList.add("blured");
+    //       this.addlargephotoTarget.insertAdjacentHTML("beforeend", `<img src="${url_image}" alt="" class="large-image" data-hover-picture-target="removelargephoto">`)
+    //     }, 500);
+    // })
+
+    // this.photoTarget.addEventListener("touchend", () => {
+    //   console.log("untouch");
+    //   if (this.removelargephotoTarget)  {
+    //     this.bluredTarget.classList.remove("blured");
+    //     this.removelargephotoTarget.remove();
+    //   }
+    //   clearTimeout(timer)
+    // })
+
   }
 
   addphoto(event) {
-    event.preventDefault(event);
-    console.log(event.currentTarget);
+    // let touchDuration = 0;
+    // const startTime = Date.now();
+    // console.log("start time", startTime);
     const url_image = event.currentTarget.src;
-    this.bluredTarget.classList.add("blured");
-    this.addlargephotoTarget.insertAdjacentHTML("beforeend", `<img src="${url_image}" alt="" class="large-image" data-hover-picture-target="removelargephoto">`)
-  }
+    this.timer = setTimeout((event) => {
+        this.bluredTarget.classList.add("blured");
+        this.toto = this.addlargephotoTarget.insertAdjacentHTML("beforeend", `<img id="toto" src="${url_image}" alt="" class="large-image" data-hover-picture-target="removelargephoto">`)
+      }, 300);
+    }
+
+
 
   removephoto(event){
-    event.preventDefault(event)
     console.log("untouch");
-    this.bluredTarget.classList.remove("blured");
-    this.removelargephotoTarget.remove()
-
+    const toto = document.querySelector("#toto")
+    if (document.querySelector("#toto"))  {
+      this.bluredTarget.classList.remove("blured");
+      toto.remove();
+    }
+    clearTimeout(this.timer)
   }
 }
